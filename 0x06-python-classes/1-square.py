9ecbae1ff00a1d215098b43ab3c15e9743711460
DaveOuma <davecarlosotieno@gmail.com>
2024-02-10 11:01:33 -0500

    created a class that defines a square based on 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Module: 1-square """
+
+class Square:
+    """ Defines a square """
+    def __init__(self, size):
+        """ Initializes the Square instance with a size """
+        self.__size = size
